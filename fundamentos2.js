4db1341ba17b80eb9b6938d40fee447a0739fb43
SL <susanavlr@live.cl>
2020-02-20 18:46:28 -0300

    act

--- fundamentos2.js
+++ fundamentos2.js
@@ -299,7 +299,7 @@ console.log(centro([true, 42, 'Ada', 2, 'pizza']));
 
 
 /*Escala el Array - Dado un array arr y un número num, multiplica todos los valores en el array arr por el número
-num, y devuelve el array arr modificado. Por ejemplo, escalaArray([1,2,3], 3] debería devolver [3,6,9].*/
+num, y devuelve el array arr modificado. Por ejemplo, escalaArray([1,2,3], 3] debería devolver [3,6,9].**/
 
   
 function mult(arr){
