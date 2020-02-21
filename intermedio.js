91b4964ade4c2e3b1a1003478b9e14778b1c7c48
SL <susanavlr@live.cl>
2020-02-20 22:13:54 -0300

    gag

--- intermedio.js
+++ intermedio.js
@@ -24,7 +24,7 @@ function factorial (num){
   console.log(factorial(5));
 
 //Fibonacci - Crea una función para generar números de Fibonacci. En esta famosa secuencia matemática, cada número es la
-// suma de las dos anteriores, partiendo con los valores 0 y 1. Tu función debería aceptar un argumento, un índice en la
+// suma de las dos anterqiores, partiendo con los valores 0 y 1. Tu función debería aceptar un argumento, un índice en la
 // secuencia (donde 0 corresponde al valor inicial, 4 corresponden al valor cuatro más tarde, etc). 
 //Ejemplos: fibonacci(0) = 0 (dado), fibonacci(1) = 1 (dado), fibonacci(2) = 1 (fib(0)+fib(1), o 0+1), fibonacci(3) = 
 //2 (fib(1) + fib(2)3, o 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 
